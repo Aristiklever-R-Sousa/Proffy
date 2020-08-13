203d18fcd6636735e72108813d6d41e1da08dc12
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2020-08-13 12:43:51 -0300

    Day 4 (parte 2) - Construímos as rotas (em pilha e tabs) e fizemos o componente PageHeader para a rota Study, que possuí subrotas (proffys e favoritos)

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -10,6 +12,16 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 
 function Landing() {
+    const { navigate } = useNavigation();
+
+    function handleNavigateToGiveClassesPage() {
+        navigate('GiveClasses');
+    }
+
+    function handleNavigateToGiveStudyPages() {
+        navigate('Study');
+    }
+
     return (
         <View style={styles.container} >
             <Image source={landingImg} style={styles.banner} />
@@ -20,17 +32,23 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton
+                    onPress={handleNavigateToGiveStudyPages}
+                    style={[styles.button, styles.buttonPrimary]}
+                >
                     <Image source={studyIcon} />
 
                     <Text style={styles.buttonText}>Estudar</Text>
-                </TouchableOpacity>
+                </RectButton>
 
-                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                <RectButton
+                    onPress={handleNavigateToGiveClassesPage}
+                    style={[styles.button, styles.buttonSecondary]}
+                >
                     <Image source={giveClassesIcon} />
 
                     <Text style={styles.buttonText}>Dar aulas</Text>
-                </TouchableOpacity>
+                </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
