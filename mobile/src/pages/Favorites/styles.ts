5fba5302c125c2b68f9f1a55768a021fb87ad480
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2020-08-14 18:28:57 -0300

    Day 5 - Finalizamos as funcionalidades da aplicação mobile: conectamos com o server, listamos os proffys e criamos o sistema de favoritos.

--- mobile/src/pages/Favorites/styles.ts
+++ mobile/src/pages/Favorites/styles.ts
@@ -4,8 +4,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#f0f0f7'
-    }
+    },
 
+    teacherList: {
+        marginTop: -40
+    }
 });
 
 export default styles;
